e632e5823d71d8552b50cdae17ec6bef6474f355
armod <armods@gmail.com>
2022-12-23 15:25:59 +0100

    mongoose validation

--- controllers/main.js
+++ controllers/main.js
@@ -2,11 +2,10 @@ const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
   const { username, password } = req.body
+  // mongoose validation
   if (!username || !password) {
     throw new CustomAPIError('Please provide email and password', 400)
   }
-  // mongo
-  // Joi
 
   console.log(username, password)
   res.send('Fake Login/Register/Signup Route')
